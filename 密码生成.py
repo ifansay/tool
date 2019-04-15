5edd3a32a99bbc6141a31782df7b0abb70e7ac0a
ifansay Zhang <ifansay.chn@qq.com>
2019-04-15 18:03:33 +0800

    Update 密码生成.py
    修改密码校验方式

--- 密码生成.py
+++ 密码生成.py
@@ -12,33 +12,56 @@ import string
 import random
 import datetime
 import pathlib
+import os
+import winsound
+from functools import reduce
 
 
-def passwordProduct(long):
-    src = string.ascii_letters + string.digits + string.punctuation
-    # 此处亦可以用正则表达式校验
-    password = random.sample(src, sorted([5, long-3, 32])[1])  # 随机取n位,排序取中值
-    password.extend(random.sample(string.digits, 1))  # 让密码中一定包含数字
-    password.extend(random.sample(string.ascii_lowercase, 1))  # 让密码中一定包含小写字母
-    password.extend(random.sample(string.ascii_uppercase, 1))  # 让密码中一定包含大写字母
-    for i in range(10000):
-        random.shuffle(password)  # 打乱列表顺序
-    str_password = ''.join(password)  # 将列表转化为字符串
-    return str_password
+worddict = {'1': string.digits,
+            '2': string.ascii_lowercase,
+            '3': string.ascii_uppercase,
+            '4': string.punctuation
+            }
+
+
+def infoInput():
+    tags = input('请输入账号归属(最长10位,英文):')[:10]
+    account = input('请输入账号(最长30位,英文):')[:30]
+    passlong = int(input('请输入密码长度:'))
+    mode = input('请输入密码组成类型\n   1.数字\n   2.小写字母\n   3.大写字母\n   4.英文标点\n')
+    return tags, account, passlong, mode
 
 
 def longCheck(long):
     if long < 8:
-        print('密码最短8位')
+        print('密码最短6位')
     elif long > 35:
         print('密码最长35位')
 
 
+def passwordProduct(mode, long):
+    src = ''
+    for no in mode:
+        src += worddict[no]
+    password = random.sample(src, sorted([6, long, 35])[1])  # 随机取n位,排序取中值
+    str_password = ''.join(password)  # 将列表转化为字符串
+    return str_password
+
+
+def passwordCheck(mode, password):
+    wordcheck = {}
+    for no in mode:
+        wordcheck[no] = len([i for i in password if i in worddict[no]])
+    VerifyResult = reduce(lambda x, y: x * y, wordcheck.values())
+    return VerifyResult
+
+
 def passwordSave(tags, account, password, date):
-    file = '%s密码.txt' % tags
+    path, code_file = os.path.split(os.path.realpath(__file__))
+    file = '%s\\password\\%s密码.txt' % (path, tags)
     fileCheck = pathlib.Path(file).is_file()
     f = open(file, 'a+', encoding='utf-8')
-    if not fileCheck:  # 确认文件是否存在的判断规则
+    if not fileCheck:
         f.writelines('App&Web'.ljust(15, ' '))
         f.writelines('AccountNo'.ljust(35, ' '))
         f.writelines('Passwords'.ljust(40, ' '))
@@ -46,27 +69,27 @@ def passwordSave(tags, account, password, date):
     f.writelines(tags.ljust(15, ' '))
     f.writelines(account.ljust(35, ' '))
     f.writelines(password.ljust(40, ' '))
-    f.writelines(date)
+    f.writelines(date.strftime('%Y/%m/%d %H:%M:%S'))
     f.writelines('\n')
     f.close()
 
 
-def infoInput():
-    tags = input('请输入账号归属(最长10位,英文):')[:10]
-    account = input('请输入账号(最长30位,英文):')[:30]
-    passlong = int(input('请输入密码长度:'))
-    return tags, account, passlong
-
-
 def main():
     try:
-        tags, account, long = infoInput()
+        tags, account, long, mode = infoInput()
         longCheck(long)
-        password = passwordProduct(long)
-        passwordSave(tags, account, password, datetime.datetime.today().strftime('%Y/%m/%d %H:%M:%S'))
-        print(tags, "的密码为:", password, '\n密码已存入文件')
+        while True:
+            word = passwordProduct(mode, long)
+            result = passwordCheck(mode, word)
+            if result > 0:
+                passwordSave(tags, account, word, datetime.datetime.today())
+                print(tags, "的密码为:", word, '\n密码已存入文件')
+                break
     except:
-        print('输入有误')
+        print('error')
 
 
-main()
+if __name__ == '__main__':
+    main()
+    winsound.Beep(1000, 300)  # 其中400表示声音大小,1000表示发生时长,1000为1秒
+    # input('\n\n请按任意键退出')
