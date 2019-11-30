54c832a095396aafc2a8ffe4074dd95c24ee1a20
杜学邹 <ifansay.chn@qq.com>
2019-11-30 23:22:03 +0800

    Update excel拆分.py
    如果mail文件夹存在文件，提示

--- 脚本/excel拆分.py
+++ 脚本/excel拆分.py
@@ -1,12 +1,13 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Wed Feb 13 11:08:28 2019
 
 @author: 饭未眠
+
+@email: ifansay.chn@qq.com
 """
 import os
 import os.path
-import datetime
 import time
 import shutil
 import xlrd
@@ -18,6 +19,7 @@ import random
 import winsound
 import configparser as cp
 
+# 如果excel内容报错,拆分会陷入死循环 #
 # comment
 """版本号v2.7,更新日期2018-12-13
 1.增加对单一文件的多次拆分支持"""
@@ -35,8 +37,8 @@ config = cp.RawConfigParser()
 file_path, code_file = os.path.split(os.path.realpath(__file__))
 
 # 读取文件路径
-config.read(file_path+'\\sf_config.ini', encoding='utf-8')
-sf_config = config['DEFAULT']
+config.read(file_path+'//config.ini', encoding='utf-8')
+sf_config = config['default']
 path_project = sf_config['project']
 path_file = sf_config['file']
 path_filed = sf_config['filed']
@@ -45,16 +47,15 @@ path_mailed = sf_config['mailed']
 separator = sf_config['separator']
 
 
-def time_to_str():
-    no = datetime.datetime.today().strftime('%y%m%d')
-    return no
+def time_no():
+    return time.strftime("%Y%m%d", time.localtime())
 
 
 def move(file, path):
     src = string.ascii_letters+string.digits
     randstr = ''.join(random.sample(src, 6))
     shotname, extension = os.path.splitext(file)
-    shutil.move(file, path+"\\"+shotname+'_SO'+'_'+randstr+extension)
+    shutil.move(file, path+"//"+shotname+'_SO'+'_'+randstr+extension)
 
 
 def textSplit(text, split, data, title, sheet='sheet'):
@@ -113,7 +114,7 @@ def csvRead(file, split_no):
         text = re.sub('[\r\n\t"]', '', t).split(',')
         if n == 1:
             title = text
-            print('拆分的列:', [title[int(i)] for i in split_no])
+            print('%s拆分的列:' % file, [title[int(i)] for i in split_no])
         else:
             data = textSplit(text, split_no, data, title)
         n += 1
@@ -127,11 +128,11 @@ def excelRead(file, split_no):
     for sheet in book.sheets():
         try:
             title = sheet.row_values(0)
-            print('拆分的列:', [title[int(i)] for i in split_no])
+            print('%s拆分的列:' % sheet.name, [title[int(i)] for i in split_no])
             for row in range(1, sheet.nrows):
                 text = sheet.row_values(row)
                 text_type = sheet.row_types(row)
-                # get datatype,type: 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
+                # get datatype,type: 0empty,1string, 2number, 3date, 4boolean, 5error
                 text = list(map(textFormat, text_type, text))
                 data = textSplit(text, split_no, data, title, sheet=sheet.name)
         except IndexError:
@@ -141,19 +142,18 @@ def excelRead(file, split_no):
 
 def excelWrite(path, data, name, separator):  # data {city:{sheet:data}}
     top = {"font_name": u'微软雅黑',
-           'border': 4,
-           'border_color': '50616d',
-           'font_size': 11,
-           'bold': True,
-           'bg_color': 'fffbf0'}
+        'border': 4,
+        'border_color': '50616d',
+        'font_size': 11,
+        'bold': True,
+        'bg_color': 'fffbf0'}
     oth = {"font_name": u'微软雅黑',
-           'border': 4,
-           'border_color': '50616d',
-           'font_size': 10}
+        'border': 4,
+        'border_color': '50616d',
+        'font_size': 10}
     for city, data_city in data.items():
         print(city)
-        workbook = xlsxwriter.Workbook(str(path+'\\%s'+separator+name+'.xlsx') %
-                                       re.sub('[\:\/\|\<\>\*\"\?\r\n\t\\"]', '', (','.join([str(i) for i in city]))))
+        workbook = xlsxwriter.Workbook(str(path+'//%s'+separator+name+'.xlsx') % re.sub('[\:\/\|\<\>\*\"\?\r\n\t//"]', '', (','.join([str(i) for i in city]))))
         "文件名是第一层data的key值的逗号连接文本"
         for sheetname, data_w in data_city.items():
             worksheet = workbook.add_worksheet(name=sheetname)
@@ -170,8 +170,8 @@ def excelWrite(path, data, name, separator):  # data {city:{sheet:data}}
 def main(file, path_mail=path_mail, path_filed=path_filed, separator=separator):
     shotname, extension = os.path.splitext(file)
     winsound.Beep(220, 800)
-    print('正在拆分 %s' % file, end='')
-    time_no = time_to_str()
+    print('\n正在拆分 %s' % file, end='')
+    time_id = time_no()
     try:
         while True:
             code = input('请输入要拆分的列(从0列开始,多列请用英文逗号分开):').split(',')
@@ -182,15 +182,15 @@ def main(file, path_mail=path_mail, path_filed=path_filed, separator=separator):
                 elif extension in ('.csv'):
                     data = csvRead(file, code)
                 print('loading...')
-                excelWrite(path_mail, data, shotname+"_S"+time_no, separator)
+                excelWrite(path_mail, data, shotname+"_S"+time_id, separator)
                 time_end = time.time()
                 print('用时 %.2f 秒' % (time_end-time_start))
             except ValueError:
                 break
         st = input('请对原文件重命名:')
         if st.strip(' '):
-            shutil.copyfile(file, path_mail+'\\'+st+separator+shotname+"_S"+time_no+extension)
-        shutil.move(file, path_filed+'\\'+shotname+"_SO"+time_no+extension)
+            shutil.copyfile(file, path_mail+'//'+st+separator+shotname+"_S"+time_id+extension)
+        shutil.move(file, path_filed+'//'+shotname+"_SO"+time_id+extension)
         winsound.Beep(1000, 800)
     except FileNotFoundError:
         print('文件不存在', file)
@@ -200,12 +200,22 @@ def main(file, path_mail=path_mail, path_filed=path_filed, separator=separator):
         print('数据错误', file)
 
 
-os.chdir(path_file)
-file_list = os.listdir('.')
-
+os.chdir(path_mail)
+mail_list = [i for i in os.listdir('.') if i not in ['.DS_Store','desktop.ini']]
 if __name__ == '__main__':
+    print('\n^_^欢迎使用智文文件助手^_^\n')
+    os.chdir(path_file)
+    file_list = [i for i in os.listdir('.') if i not in ['.DS_Store','desktop.ini']]
     if file_list:
-        print('^_^欢迎使用智文文件助手^_^\n')
-        list(map(main, file_list))
+        confirm_value = 'no'
+        if len(mail_list)>0:
+            confirm_value = input('mail文件夹存在文件,是否继续.确认继续请输入"yes":')
+        if confirm_value.lower() == "yes" or len(mail_list)==0:
+            list(map(main, file_list))
+        else:
+            print('取消了操作')
+    else:
+        print('空文件,无需拆分')
+
 
 # input('\npress any key to exit')
